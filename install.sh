4f998d87d6c0ed9982e3b30d08b74fccfa80c7bd
Khaos Tian <khaos.tian@gmail.com>
2016-11-28 12:22:44 -0800

    Update install script to take in admin username and password Update readme Update example projects

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-compile(){
+function compile(){
   COMMON_LIBS="cpp/common/admin.cpp cpp/common/errors.cpp cpp/common/evalstates.cpp cpp/common/evaluation.cpp cpp/common/filelock.cpp cpp/common/http.cpp cpp/common/judges.cpp cpp/common/projects.cpp cpp/common/rooms.cpp cpp/common/utils.cpp"
   g++ -std=c++0x -Wall -g -I ./ -o cgi-bin/admin_auth.cgi cpp/admin_auth.cpp $COMMON_LIBS
   g++ -std=c++0x -Wall -g -I ./ -o cgi-bin/admin_reset_password.cgi cpp/admin_reset_password.cpp $COMMON_LIBS
@@ -21,6 +21,32 @@ compile(){
   g++ -std=c++0x -Wall -g -I ./ -o cgi-bin/admin_remove_project.cgi cpp/admin_remove_project.cpp $COMMON_LIBS
   g++ -std=c++0x -Wall -g -I ./ -o cgi-bin/admin_remove_room.cgi cpp/admin_remove_room.cpp $COMMON_LIBS
 }
+
+function setupAdmin(){
+  echo "Setup Admin"
+  read -p "Admin username (default: admin):" username
+  if [ "$username" = "" ]; then
+    username="admin"
+  fi
+
+  read -p "Admin password (default: password):" password
+  if [ "$password" = "" ]; then
+    password="password"
+  fi
+
+  read -p "Admin email (default: nmatsunaga@scu.edu):" email
+  if [ "$email" = "" ]; then
+    email="nmatsunaga@scu.edu"
+  fi
+
+  token=$(hexdump -n 3 -v -e '/1 "%02X"' /dev/urandom)
+  reset_token=$(hexdump -n 6 -v -e '/1 "%02X"' /dev/urandom)
+
+  cat > cgi-bin/Config/admin.txt <<-EOF
+$username,$password,$token,$email,$reset_token
+EOF
+}
+
 echo "Untarring the Tarball"
 tarname=judgeEval.tar.gz
 tar -xzvf $tarname
@@ -28,13 +54,14 @@ rm $tarname
 echo "Creating cgi-bin directory"
 mkdir cgi-bin
 mv Sources/cpp cpp
-echo "Begun compilation"
+echo "Begun compilation - this may takes a while..."
 compile
 rm -r cpp
 echo "Compilation complete, creating schema directories"
 mv Config cgi-bin/Config
 mkdir cgi-bin/Eval
 mv Sources/static/* ./
+setupAdmin
 echo "Configuring permissions"
 chmod 600 cgi-bin/Config/*
 chmod 700 cgi-bin/*.cgi
---
+++ readme.txt
@@ -0,0 +1,12 @@
+FILENAME:readme.txt
+TITLE:Installation Instructions for Judge Eval System
+AUTHORS:Maor Bernstein,Tian Zhang, and Wilson Burchenal
+
+1. Copy all the files to the server
+2. In terminal, first run construct_tar.sh
+3. After finished, copy judgeEval.tar.gz and install.sh to the root directory (For ECC, it should be /webpages/{username}/) of the website
+4. In the website's root directory, run install.sh
+5. Delete install.sh
+6. After finished, you can visit the admin portal at http://{url to your website}/admin.html and the judge interface at http://{url to your website}/eval.html
+
+You can find sample projects for import under Resources/project_examples.csv
